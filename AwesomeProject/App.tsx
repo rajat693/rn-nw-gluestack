99427ebd34ecee01623f56efc7f447bf7a748107
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-04-09 13:36:09 +0530

    feat: added nativewind setup

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './global.css';
+import {Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">hello world</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
